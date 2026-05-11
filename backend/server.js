30658f429c4f7c9ae400ae31d764779bbbcc1299
aditya <pradumy782@gmail.com>
2026-05-11 09:21:31 +0530

    Fix syntax error in server.js missing brace

--- backend/server.js
+++ backend/server.js
@@ -128,6 +128,7 @@ app.get('/api/option-chain', async (req, res) => {
                          }
                      });
                  }
+                 }
              }
          }
      }
